3151ed88aaed3ca8178a7a30d62ab5912c2cc099
BashaVali <bashavali222@gmail.com>
2022-11-24 04:01:41 +0530

    Stack And Queue

--- StackAndQueue/Program.cs
+++ StackAndQueue/Program.cs
@@ -13,9 +13,11 @@ namespace StackAndQueue
             Console.WriteLine("Welcome To Stack And Queue Program");
             Console.WriteLine("1.Push operation");
             Console.WriteLine("2.Peek And Pop");
+            Console.WriteLine("3.Enqueue");
             Console.WriteLine("Enter the option");
             int num = Convert.ToInt32(Console.ReadLine());
             Stack stack = new Stack();
+            Queue queue = new Queue();
             switch (num)
             {
                 case 1:
@@ -31,6 +33,13 @@ namespace StackAndQueue
                     stack.Pop();
                     stack.Display();
                     break;
+                case 3:
+                    queue.Enqueue(56);
+                    queue.Enqueue(30);
+                    queue.Enqueue(70);
+                    queue.Display();
+                    break; 
+
                 default:
                     Console.WriteLine("Enter a valid option");
                     break;
